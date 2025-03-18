62a2831f68ef85948f3a85a12a227aefb4bfb3c1
devray041 <145239195+devray041@users.noreply.github.com>
2025-03-17 23:31:28 -0300

    Update app.js
    Primeira funcionalidade: enviar um alerta caso o botão "Adicionar" seja apertado sem nenhum nome ter sido inserido pelo usuário. Caso o nome tenha sido inserido, ele é armazenado numa lista.

--- Desafio/app.js
+++ Desafio/app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let ListaNomes[];
+
+function AdicionarAmigo() {
+  let amigoAdicionado = document.getElementById("amigo").value;
+  
+  if (amigoAdicionado == '') {
+      alert('Por favor, insira um nome válido no campo "Digite um nome"')};
+  else {
+    listaNomes.push(amigoAdicionado)};
+  
+}
+
